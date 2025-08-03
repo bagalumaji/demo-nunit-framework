d8d5500b8575f78b9cac2976b917a992091e1fb0
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-03 11:48:20 +0530

    order attribute

---
+++ OrderDemoTests.cs
@@ -0,0 +1,32 @@
+﻿namespace demo_nunit_framework
+{
+    public class OrderDemoTests
+    {
+        // execute from cmd - donet test --filter Name=OrderDemoTests
+        [Test,Order(2)]
+        public void OrderDemoTest1()
+        {
+            Console.WriteLine("OrderDemoTest1 executed...");
+        }
+        [Test, Order(3)]
+        public void OrderDemoTest2()
+        {
+            Console.WriteLine("OrderDemoTest 2 executed...");
+        }
+        [Test, Order(5)]
+        public void OrderDemoTest3()
+        {
+            Console.WriteLine("OrderDemoTest 3 executed...");
+        }
+        [Test, Order(2)]
+        public void OrderDemoTest4()
+        {
+            Console.WriteLine("OrderDemoTest 4 executed...");
+        }
+        [Test,Order(1)]
+        public void OrderDemoTest5()
+        {
+            Console.WriteLine("OrderDemoTest 5 executed...");
+        }
+    }
+}
