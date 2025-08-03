361589caa0791ab6d62258c4cee3a8eef51d3bff
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-03 10:15:05 +0530

    Add project files.

---
+++ Demo.cs
@@ -0,0 +1,12 @@
+namespace demo_nunit_framework
+{
+    public class DemoTests
+    {
+       
+        [Test]
+        public void DemoTest()
+        {
+            Console.WriteLine("demo test");
+        }
+    }
+}
