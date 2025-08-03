99834558d452d689aa5d1fb57e54e04aa416890f
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-03 11:57:35 +0530

    added test data class

---
+++ TestDataProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace demo_nunit_framework
+{
+    public class TestDataProvider
+    {
+        public static IEnumerable<TestCaseData> GetTestData()
+        {
+            yield return new TestCaseData("A", "B", "C");
+            yield return new TestCaseData("D", "E", "F");
+        }
+    }
+}
