bfd66f1224123596102c487633b9bc96adbce9da
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-03 10:20:55 +0530

    test with setup and teardown

---
+++ TestWithSetupAndTeardown.cs
@@ -0,0 +1,21 @@
+﻿namespace demo_nunit_framework
+{
+    public class TestWithSetupAndTeardown
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup: This runs before each test.");
+        }
+        [Test]
+        public void SetupAndTearDownTest()
+        {
+            Console.WriteLine("Test1: This is the first test.");
+        }
+        [TearDown]
+        public void Teardown()
+        {
+            Console.WriteLine("Teardown: This runs after each test.");
+        }
+    }
+}
