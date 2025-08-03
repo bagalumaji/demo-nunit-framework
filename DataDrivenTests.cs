cac6b87114870ec5925898c072bd0257d5472fbe
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-03 11:30:44 +0530

    added ignoring codition

--- DataDrivenTests.cs
+++ DataDrivenTests.cs
@@ -4,11 +4,25 @@
     {
         [TestCase(1, 2, 3)]
         [TestCase(4, 5, 9)]
-        public void DemoTest(int a,int b,int c)
+        [Ignore("Flaky")]
+        public void DemoTest(int a, int b, int c)
         {
-            Console.WriteLine("first Value : {0}",a);
-            Console.WriteLine("seconds value : {0}",b);
-            Console.WriteLine("third value : {0}",c);
+            Console.WriteLine("first Value : {0}", a);
+            Console.WriteLine("seconds value : {0}", b);
+            Console.WriteLine("third value : {0}", c);
         }
+
+        [TestCaseSource(nameof(GetTestData))]
+        public void DemoTest2(string a, string b, string c)
+        {
+            Console.WriteLine("first Value : {0}", a);
+            Console.WriteLine("seconds value : {0}", b);
+            Console.WriteLine("third value : {0}", c);
+        }
+        static readonly object[] GetTestData =
+        {
+            new object[] { "A","B","C"},
+            new object[] { "D","E","F"}
+        };
     }
 }
