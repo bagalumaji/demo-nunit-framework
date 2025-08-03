76224c9611e224fe178d43f063f677c39da11f5e
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-03 11:13:58 +0530

    repeat  and category

---
+++ RepeatCategoryTests.cs
@@ -0,0 +1,24 @@
+﻿namespace demo_nunit_framework
+{
+
+    [TestFixture]
+    public class RepeatCategoryTests
+    {
+        [Category("Sanity")]
+        [Repeat(2)]
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("test 1 executed...");
+            Assert.Pass("Test1 executed successfully.");
+        }
+        // 2nd test with the same category
+        [Test,Category("Smoke"),Repeat(3)]
+        public void Test2()
+        {
+            Console.WriteLine("test 1 executed...");
+            Assert.Pass("Test1 executed successfully.");
+        }
+        
+    }
+}
