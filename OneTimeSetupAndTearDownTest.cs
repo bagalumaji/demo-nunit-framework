3137cab878597478a63770d282c95016cfda6983
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-08-03 10:30:53 +0530

    1 time setup and teardown

---
+++ OneTimeSetupAndTearDownTest.cs
@@ -0,0 +1,28 @@
+﻿namespace demo_nunit_framework
+{
+    public class OneTimeSetupAndTearDownTest
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Console.WriteLine("one time setup executed...");
+        }
+        [Test]
+        public void demo1Test()
+        {
+            Console.WriteLine("demo test 1 executed...");
+        }
+
+        [Test]
+        public void demo2Test()
+        {
+            Console.WriteLine("demo test 2 executed...");
+        }
+      
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Console.WriteLine("one time teardown executed...");
+        }
+    }
+}
